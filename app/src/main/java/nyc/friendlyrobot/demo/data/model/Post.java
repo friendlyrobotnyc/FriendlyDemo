7b515fa2f6dc00dbfdade945b431dd6d83be1698
digitalbuddha <mnakhimovich@gmail.com>
2016-11-17 18:54:03 -0500

    chuckdb

--- app/src/main/java/nyc/friendlyrobot/demo/data/model/Post.java
+++ app/src/main/java/nyc/friendlyrobot/demo/data/model/Post.java
@@ -7,24 +7,25 @@ import com.google.common.base.Optional;
 import org.immutables.value.Value;
 
 @Value.Immutable
-@Value.Style(allParameters = true)
 public abstract class Post implements PostModel {
-    public static final Mapper<Post> MAPPER =
-            new Mapper<>((Mapper.Creator<Post>) ImmutablePost::of);
-
-    public static final class Marshal extends PostMarshal {
-    }
-
-    @Value.Parameter(false)
     @Nullable
     public abstract Preview preview();
 
-    @Value.Parameter(false)
+    abstract String title();
+
+    abstract String url();
+
+    @Nullable
+    abstract Integer height();
+
+    @Nullable
+    abstract Integer width();
+
     @Value.Derived
     public Optional<Image> nestedThumbnail() {
-        if(preview()==null||preview().images()==null||preview().images().get(0).source()==null)
-        return Optional.absent();
-            return Optional.of(preview().images().get(0).source());
+        if (preview() == null || preview().images() == null || preview().images().get(0).source() == null)
+            return Optional.absent();
+        return Optional.of(preview().images().get(0).source());
     }
 
 }
--- app/src/main/java/nyc/friendlyrobot/demo/interaction/RedditReader.java
+++ app/src/main/java/nyc/friendlyrobot/demo/interaction/RedditReader.java
@@ -6,45 +6,33 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import nyc.friendlyrobot.demo.data.model.Children;
 import nyc.friendlyrobot.demo.data.model.Post;
+import nyc.friendlyrobot.demo.data.model.RedditData;
 import nyc.friendlyrobot.demo.data.store.RedditStore;
 import rx.Observable;
 import rx.schedulers.Schedulers;
-
-import static nyc.friendlyrobot.demo.data.model.Post.TABLE_NAME;
 
 public class RedditReader extends Interaction {
     public static final String LIMIT = "50";
 
     private final RedditStore store;
-    private final RedditSaver saver;
 
     @Inject
-    public RedditReader(RedditStore store, RedditSaver saver) {
+    public RedditReader(RedditStore store) {
         this.store = store;
-        this.saver = saver;
     }
 
-    public Observable<List<Post>> readPosts() {
-        if (!recordExists(Post.TABLE_NAME, Post._ID, "1")) {
-            return seedDB()
-                    .flatMap(posts -> getPostsFromDB());
-        }
-        return getPostsFromDB();
+    public Observable<List<Post>> posts() {
+        return store.get(LIMIT)
+                .subscribeOn(Schedulers.io())
+                .flatMap(this::posts)
+                .toList();
+
     }
 
     @NonNull
-    private Observable<List<Post>> getPostsFromDB() {
-        return db.get().createQuery(TABLE_NAME, Post.SELECT_ALL)
-                .mapToList(Post.MAPPER::map)
-                .first();
-    }
-
-    private Observable<List<Post>> seedDB() {
-        return store.get(LIMIT)
-                .subscribeOn(Schedulers.io())
-                .map(redditData -> redditData.data().children())
-                .flatMap(saver::save);
-
+    private Observable<Post> posts(RedditData redditData) {
+        return Observable.from(redditData.data().children()).map(Children::data);
     }
 }
